ef8729f1d568e43c35d0c33da398cc06204d6310
pjs21s <jsgd1254@gmail.com>
2019-04-13 00:17:57 +0900

    메인 페이지 글 클릭했을 때 이동가능 하게 수정

--- Project/versio/posts/models.py
+++ Project/versio/posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 User = get_user_model()
-import misaka
+
 # Create your models here.
 
 class Post(models.Model):
