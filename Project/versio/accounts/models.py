c3cab2f3ce4a42ed2fc207a918af43cef028e4bb
Vors <jsgd1254@gmail.com>
2019-05-11 17:12:55 +0900

    프로필 활동점수 기능 추가

--- Project/versio/accounts/models.py
+++ Project/versio/accounts/models.py
@@ -3,6 +3,7 @@ from django.contrib import auth
 from django.utils import timezone
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.signals import user_logged_in
 
 # Create your models here.
 class User(auth.models.User, auth.models.PermissionsMixin):
@@ -14,6 +15,7 @@ class Profile(models.Model):
     user = models.OneToOneField(auth.models.User, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(max_length=300, blank=True)
     lang = models.CharField(max_length=20, blank=True)
+    logined_count = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.user.username
@@ -27,6 +29,12 @@ class Profile(models.Model):
             }
         )
 
+def login_user(sender, request, user, **kwargs):
+    user.profile.logined_count = user.profile.logined_count + 1
+    user.profile.save()
+
+user_logged_in.connect(login_user)
+
 @receiver(post_save, sender=auth.models.User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
@@ -35,3 +43,4 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=auth.models.User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
